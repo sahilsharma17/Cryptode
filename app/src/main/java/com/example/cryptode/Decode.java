9eea8962ee9a1e236437aeea3a3e47bc77401494
sahilsharma17 <sahils17sam@gmail.com>
2023-03-25 19:56:14 +0530

    Added Encoder code

--- app/src/main/java/com/example/cryptode/Decode.java
+++ app/src/main/java/com/example/cryptode/Decode.java
@@ -1,4 +1,64 @@
 package com.example.cryptode;
 
+import static java.lang.Math.pow;
+
+
 public class Decode {
+    public static String decrypt(String s){
+        String invalid = "This code was not encrypted by Cryptode";
+        String initializer = "111111111";
+        Boolean flag = true;
+        for (int i = 0; i < 8; i++) {
+            if (initializer.charAt(i) != s.charAt(i)){
+                flag = false;
+                break;
+            }
+        }
+        
+        String data = "";
+        for (int i = 8; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            data = data.concat(String.valueOf(temp));
+        }
+        int asc[][] = new int[11101][8];
+        int idx=1, idx2=0;
+        for (int i = 0; i < data.length(); i++) {
+            if (i%7==0) {
+                idx++;
+                idx2 = 0;
+                char temp = data.charAt(i);
+                asc[idx][idx2] = temp - '0';
+                idx2++;
+            }else {
+                char temp = data.charAt(i);
+                asc[idx][idx2] = temp - '0';
+                idx2++;
+            }
+        }
+
+        int num[] = new int[11111];
+        int nidx=0, temp=0, count=0;
+        for (int i = 0; i <= idx ; i++) {
+            count = 0;
+            temp = 0;
+            for (int j = 6; j >=0 ; j--) {
+                int temp1 = (int)pow(2,count);
+                temp += (asc[i][j]*temp1);
+                count++;
+            }
+            num[nidx++] = temp;
+
+        }
+        String rev = "";
+        char ch;
+        for (int i = 0; i < nidx; i++) {
+            ch = (char)num[i];
+            rev = rev.concat(String.valueOf(ch));
+        }
+        if (data.length() %7 == 0 && flag == true){
+            return rev;
+        }else{
+            return invalid;
+        }
+    }
 }
--- app/src/main/java/com/example/cryptode/Encode.java
+++ app/src/main/java/com/example/cryptode/Encode.java
@@ -1,4 +1,43 @@
 package com.example.cryptode;
 
 public class Encode {
+    public static String encrypt(String s) {
+        String initializer = "111111111";
+        int count = 0;
+        int a[] = new int[111111111];
+        for (int i = 0; i < s.length(); i++) {
+            a[i] = (int)s.charAt(i);
+            count++;
+        }
+
+        String res= "";
+        int bin[] = new int[111];
+        int idx = 0;
+        for (int i = 0; i < count; i++) {
+            int temp = a[i];
+            for (int j = 0; j < count; j++) { bin[j] = 0;
+                idx = 0;
+                while (temp>0){
+                    bin[idx++] = temp % 2;
+                    temp /= 2;
+                }
+            }
+            String dig="";
+            String temps;
+            for (int j = 0; j < 7; j++) {
+                temps = Integer.toString(bin[j]);
+                dig = dig.concat(temps);
+            }
+            
+            String rev = "";
+            for (int j = dig.length(); j >= 0 ; j--) {
+                char ca = dig.charAt(j);
+                rev = rev.concat(String.valueOf(ca));
+            }
+
+            res = res.concat(rev);
+        }
+        res = initializer.concat(res);
+        return res;
+    }
 }
